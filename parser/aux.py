ce97d8360006aeba365455070c77fcff4e6ed64c
kiafas-k <38371871+kiafas-k@users.noreply.github.com>
2018-05-17 18:50:22 +0300

    Add files via upload

---
+++ parser/aux.py
@@ -0,0 +1,189 @@
+import MySQLdb
+from nltk.corpus import stopwords
+from nltk import sent_tokenize, ne_chunk, ne_chunk_sents, pos_tag, word_tokenize
+from nltk import PorterStemmer
+
+
+def dbexec(operation_type, query):
+
+    result = {
+        'success': True,
+        'data': (),
+        'rows_returned': 0,
+
+    }
+
+    if not operation_type in['insert', 'update', 'delete', 'select']:
+        result['success'] = False
+    else:
+        db = MySQLdb.connect("10.0.0.108", "viper", "d9180pa", "articles",)
+        db.set_character_set('utf8')
+        cursor = db.cursor()
+
+        try:
+            if operation_type == 'select':
+                cursor.execute(query)
+                result['data'] = cursor.fetchall()
+                result['rows_returned'] = cursor.rowcount
+
+            if operation_type == 'insert':
+                cursor.execute(db.escape_string(query))
+                db.commit()
+
+        except:
+            result['success'] = False
+
+        db.close()
+
+    return result
+
+
+def savePage(filename, content):
+    try:
+        fl = open(filename, 'w')
+        fl.writelines(content)
+        fl.close
+        result = True
+    except:
+        result = False
+
+    return result
+
+
+def measureCorpus(text):
+    sentenses = sent_tokenize(text)
+    words = word_tokenize(text)
+
+    return [sentenses, words]
+
+
+def filterText(text, stemmed=False):
+    if stemmed:
+        porter = PorterStemmer()
+
+    # remove stopwords and useless words
+    stop_words = set(stopwords.words('english'))
+    words = word_tokenize(text)
+
+    valuable_words = []
+    for wrd in words:
+        if len(wrd) > 3 and not wrd in stop_words:
+            if stemmed:
+                valuable_words.append(porter.stem(wrd.lower()))
+            else:
+                valuable_words.append(wrd.lower())
+
+    return valuable_words
+
+
+def createSummary(text_title, text, ratio):
+
+    # filter title
+    filtered_title = filterText(text_title, True)
+
+    # build stemmed -> frequency table
+    stemmed_frquency_table = {}
+
+    valuable_words = filterText(text, True)
+
+    for wrd in valuable_words:
+
+        if wrd in stemmed_frquency_table:
+            stemmed_frquency_table[wrd] += 1
+        else:
+            stemmed_frquency_table[wrd] = 1
+
+    # assign score to each sentence
+    sentenses = sent_tokenize(text)
+    sentense_scoreboard = []
+    total_score = 0
+
+    for sentense in sentenses:
+        sentense_score = 0
+        sentense_words = filterText(sentense, True)
+        for sentense_word in sentense_words:
+            sentense_score += stemmed_frquency_table[sentense_word]
+            if sentense_word in filtered_title:
+                sentense_score += 10
+
+        sentense_scoreboard.append(list([sentense, sentense_score]))
+        total_score += sentense_score
+
+    # calculate average sentense score
+    # and determine score threshold
+    average_score = int(total_score / len(sentense_scoreboard))
+    score_mutator = int(average_score * 0.6)
+    reduced_text = ''
+
+    sentense_counter = 0
+
+    for sent in sentense_scoreboard:
+        if sent[1] >= average_score + score_mutator:
+            sentense_counter += 1
+            outputx = sent[0]
+            # reduced_text += outputx.replace('\n', '<br>')
+            reduced_text += outputx
+
+    return reduced_text
+
+
+def getSentiment(text, pos_keys, neg_keys):
+    results = {}
+    positive_keys = 0
+    negative_keys = 0
+    text_size = 0
+
+    filtered_text = filterText(text, False)
+    text_size = len(filtered_text)
+
+    for wrd in filtered_text:
+        if wrd in pos_keys:
+            positive_keys += 1
+
+        if wrd in neg_keys:
+            negative_keys += 1
+
+    percentage_positive = (positive_keys / text_size) / 100
+    percentage_negative = (negative_keys / text_size) / 100
+
+    results['positive'] = positive_keys
+    results['positive_percentage'] = percentage_positive
+    results['negative'] = negative_keys
+    results['negative_percentage'] = percentage_negative
+    results['text_size'] = text_size
+
+    return results
+
+
+def extract_entity_names(t):
+    entity_names = []
+
+    if hasattr(t, 'label') and t.label:
+        if t.label() == 'NE':
+            entity_names.append(' '.join([child[0] for child in t]))
+        else:
+            for child in t:
+                entity_names.extend(extract_entity_names(child))
+
+    return entity_names
+
+
+def NE_extract(text):
+    sentences = sent_tokenize(text)
+    tokenized_sentences = [word_tokenize(sentence) for sentence in sentences]
+    tagged_sentences = [pos_tag(sentence) for sentence in tokenized_sentences]
+    chunked_sentences = ne_chunk_sents(tagged_sentences, binary=True)
+
+    entity_names = []
+    for tree in chunked_sentences:
+        entity_names.extend(extract_entity_names(tree))
+
+    return set(entity_names)
+
+
+def removeQuotes(text):
+    quotes = ['\'', '"', '\n']
+    for quote in quotes:
+        text = text.replace(quote, '')
+
+    return text
---
+++ parser/parser.py
@@ -0,0 +1,85 @@
+import newspaper
+from aux import createSummary, filterText, getSentiment, NE_extract, dbexec, removeQuotes
+import time
+import MySQLdb
+
+# load pos neg words
+fl = open('parser/negative-words.txt', 'r')
+lines = fl.readlines()
+fl.close
+
+words_neg = []
+
+
+for wrd in lines:
+    words_neg.append(wrd.replace('\n', ''))
+
+fl = open('parser/positive-words.txt', 'r')
+lines = fl.readlines()
+fl.close
+
+words_pos = []
+
+
+for wrd in lines:
+    words_pos.append(wrd.replace('\n', ''))
+
+
+# load sources
+query = 'select * from tbl_sources where active=1 order by id'
+sources = dbexec('select', query)
+
+for src in sources['data']:
+    print('scanning source [{}]'.format(src[1]))
+
+    # download articles
+    paper = newspaper.build(
+        src[1], memoize_articles=False)
+
+    for article in paper.articles:
+
+        # if isinstance(article.title, str) and len(article.title) > 1:
+
+        article.download()
+        article.parse()
+
+        title = article.title
+        url = article.url
+        timestamp = int(time.time())
+        source = src[1]
+        keywords = ''
+        text = article.text
+        named_entities = ';'.join(NE_extract(article.text))
+        topic = src[2]
+
+        sentiment = 'neu'
+        sentiment_analysis = getSentiment(
+            article.text, words_pos, words_neg)
+        if sentiment_analysis['positive_percentage'] > sentiment_analysis['negative_percentage']:
+            sentiment = 'pos'
+        else:
+            sentiment = 'neg'
+
+        try:
+            db = MySQLdb.connect("10.0.0.108", "viper",
+                                 "d9180pa", "articles",)
+            db.set_character_set('utf8')
+            cursor = db.cursor()
+
+            text = removeQuotes(text)
+            text = str(db.escape_string(text))
+            text = text[2: - 2]
+
+            title = removeQuotes(title)
+            title = str(db.escape_string(title))
+            title = title[2: -2]
+
+            query = '''insert into tbl_articles (title,url,timestamp,source,keywords,text,named_entities,topic,sentiment) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
+
+            cursor.execute(query, (title, url, timestamp, source,
+                                   keywords, text, named_entities, topic, sentiment))
+            db.commit()
+            db.close()
+
+        except:
+            print('failed to store {}'.format(article.title))
